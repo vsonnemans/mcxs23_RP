e4e2ce9cdf7d9eb95c813bb1a7ad0817f0050506
vsonnemans <129926769+vsonnemans@users.noreply.github.com>
2023-04-05 22:55:01 +1000

    up

--- code.R
+++ code.R
@@ -1,21 +1,34 @@
 library(readrba)
-library(readabs)
-library(tseries)
 library(xts)
+library(tseries)
+library(urca)
+library(FinTS)
+library(rmarkdown)
 
 #Our variables
 #Household final consumption expenditures 
-pce.dl<- read_rba(series_id = "GGDPECCVPSH")
+pce.dl<-read_rba(series_id = "GGDPECCVPSH")
+pce<-to.quarterly(xts(pce.dl$value, pce.dl$date), 
+                         OHLC = FALSE)
 
 #Real household disposable income
-pce.dl<- read_rba(series_id = "GGDPICHRDI")
+inc.dl<-read_rba(series_id = "GGDPICHRDI")
+inc<-to.quarterly(xts(inc.dl$value, inc.dl$date), 
+                  OHLC = FALSE)
 
 #Interest rates on three-month treasury bills 
-pce.dl<- read_rba(series_id = "FIRMMTN3D")
+int.dl<-read_rba(series_id = "FIRMMTN3D")
+int<-to.quarterly(xts(int.dl$value, int.dl$date), 
+                  OHLC = FALSE)
 
 #Consumer sentiment indicator
-pce.dl<- read_rba(series_id = "GICWMICS")
+sent.dl<-read_rba(series_id = "GICWMICS")
+sent<-to.quarterly(xts(sent.dl$value, sent.dl$date), 
+                  OHLC = FALSE)
 
+Y.df <- na.omit(merge(pce, inc, int, sent))
+varname_vec<-c("Household final consumption expenditures", "Real household disposable income", "Interest rates on three-month treasury bills","Consumer sentiment indicator" )
+colnames(Y.df)<-varname_vec
 
-
-
+plot(pce[,0], y=pce[,1], type = "l", 
+     ylab = "", xlab = "", lwd = 1.5)
